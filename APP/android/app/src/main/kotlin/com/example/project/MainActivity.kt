d436f4673684d88d6c121435f03370e2586b18f2
노규백 <shrbqor1104@naver.com>
2025-09-29 02:21:48 +0900

    Feat

--- APP/android/app/src/main/kotlin/com/example/project/MainActivity.kt
+++ APP/android/app/src/main/kotlin/com/example/project/MainActivity.kt
@@ -1,16 +1,5 @@
 package com.example.project
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity : FlutterActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        WearBridge.bind(flutterEngine.dartExecutor.binaryMessenger)
-    }
-
-    override fun detachFromFlutterEngine() {
-        super.detachFromFlutterEngine()
-        WearBridge.unbind()
-    }
-}
+class MainActivity : FlutterActivity()
