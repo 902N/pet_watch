1fa612acfbc43745f61b4b394bcbdefb46b7bfff
902N <145334908+902N@users.noreply.github.com>
2025-09-29 01:43:00 +0900

    feat: bridge wear messages to app walk posting

--- APP/android/app/src/main/kotlin/com/example/project/WearableListenerService.kt
+++ APP/android/app/src/main/kotlin/com/example/project/WearableListenerService.kt
@@ -1,50 +1,36 @@
 package com.example.project
 
-import android.os.Handler
-import android.os.Looper
+import android.util.Log
 import com.google.android.gms.wearable.MessageEvent
 import com.google.android.gms.wearable.WearableListenerService
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.embedding.engine.dart.DartExecutor
-import io.flutter.plugin.common.MethodChannel
 import org.json.JSONObject
 
 class WearableListenerService : WearableListenerService() {
-
-    private lateinit var channel: MethodChannel
-
-    override fun onCreate() {
-        super.onCreate()
-        // FlutterEngine and MethodChannel setup
-        val flutterEngine = FlutterEngine(this)
-        flutterEngine.dartExecutor.executeDartEntrypoint(
-            DartExecutor.DartEntrypoint.createDefault()
-        )
-        channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "com.example.project/wear")
-    }
-
     override fun onMessageReceived(messageEvent: MessageEvent) {
         when (messageEvent.path) {
             "/walk/start" -> {
                 val data = JSONObject(String(messageEvent.data))
-                val args = mapOf(
-                    "startEpochMs" to data.getLong("startEpochMs"),
-                    "intensity" to data.getString("intensity")
-                )
-                Handler(Looper.getMainLooper()).post {
-                    channel.invokeMethod("start_walk", args)
-                }
+                val startEpochMs = data.getLong("startEpochMs")
+                val intensity = data.getString("intensity")
+                WearBridge.sendStart(startEpochMs = startEpochMs, intensity = intensity)
             }
             "/walk/end" -> {
                 val data = JSONObject(String(messageEvent.data))
-                val args = mapOf(
-                    "endEpochMs" to data.getLong("endEpochMs"),
-                    "durationSec" to data.getInt("durationSec"),
-                    "intensity" to data.getString("intensity")
+                val endEpochMs = data.getLong("endEpochMs")
+                val durationSec = data.getInt("durationSec")
+                val intensity = data.getString("intensity")
+                WearBridge.sendEnd(
+                    endEpochMs = endEpochMs,
+                    durationSec = durationSec,
+                    intensity = intensity,
                 )
-                Handler(Looper.getMainLooper()).post {
-                    channel.invokeMethod("end_walk", args)
-                }
+            }
+            else -> {
+                Log.w(
+                    "WearListener",
+                    "Unhandled wear message path=${messageEvent.path}",
+                )
+                super.onMessageReceived(messageEvent)
             }
         }
     }
